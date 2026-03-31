488b3cada5ecc0d134a5735d5dbb3cf125836ee4
Jan <89805433+MortalRave@users.noreply.github.com>
2026-03-31 18:48:46 +0200

    Add HabitsController with initial GET endpoint

---
+++ HabitFlow/HabitFlow/Controllers/HabitsController.cs
@@ -0,0 +1,25 @@
+﻿using HabitFlow.Data;
+using HabitFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabitFlow.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HabitsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public HabitsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Habit>>> GetHabits()
+    {
+        var habits = await _context.Habits.ToListAsync();
+        return Ok(habits);
+    }
+}
